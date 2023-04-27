45c6a459a8cccd84c48c12e709bfaa302ca9d336
suyash4 <sarlimar4@gmail.com>
2023-04-27 23:17:54 +0530

    model

---
+++ src/components/model_trainer.py
@@ -0,0 +1,51 @@
+import os
+import sys
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score,confusion_matrix
+from src.utils import save_object
+from src.exception import CustomeException
+from src.logger import logging
+
+from src.utils import save_object
+from dataclasses import dataclass
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path=os.path.join('artifacts',"model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+
+    def initiate_model_trainer(self,train_array,test_array):
+        try:
+            logging.info('Splitting training and testing input')
+
+
+            X_train,y_train,X_test,y_test=(
+               train_array[:,:-1],
+               train_array[:,-1],
+               test_array[:,:-1],
+               test_array[:,-1]
+                
+                )
+            
+            model=LogisticRegression()
+            model.fit(X_train,y_train)
+            score=model.score(X_train,y_train)
+            #print(f"model score is {score}")
+            y_pred=model.predict(X_test)
+            accuracy=accuracy_score(y_test,y_pred)
+            #print(f'accuracy score is {accuracy}')
+            
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=model
+            )
+            return accuracy
+
+
+        except Exception as e:
+            raise CustomException(e,sys)
--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -2,10 +2,15 @@
 
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 if __name__=="__main__":
     data_ingestion=DataIngestion()
     train_data,test_data=data_ingestion.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_array,test_array,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer=ModelTrainer()
+    model_trainer.initiate_model_trainer(train_array,test_array)
+
